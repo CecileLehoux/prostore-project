d65b31d4087efd0d8967a0fd51a5b26a309eace6
CecileLehoux <cecile.lehoux@laposte.net>
2025-04-25 10:44:53 +0200

    prostore with ui

---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,31 @@
+import ProductCard from "./product-card";
+
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+  return (
+    <div className="my-10">
+      <h2 className="h2-bold mb-4 ">{title}</h2>
+      {data.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-3">
+          {limitedData.map((product: any) => (
+            <ProductCard key={product.slug} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center h-64">
+          <p className="text-lg">No products found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
